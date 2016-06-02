b4631b0d9b03ba6c4ed78d00a215cf094c85f786
Jesse Gao Zhang <jgzhang@alaska.edu>
2016-06-01 16:43:50 -0800

    Added naming and building a nested list
    nested lists

--- njst.R
+++ njst.R
@@ -1,6 +1,7 @@
-#2 test cases
-test<-"(((AID,BED),CAB),(DAD,EGG))"
+#3 test cases
+#test<-"(((AID,BED),CAB),(DAD,EGG))"
 #test<-"((ANC:.1,BED:.2):.15,(CAS:.1,DED:.05):.2)"
+test<-"((ABE,BOG),((COD,DON),EGO))"
 
 #phase out unecessary data in the newick notation
 simplified<-gsub("[:.0-9]","",test)
@@ -40,12 +41,43 @@ for(i in full_split)
 }
 
 #creates matrix 
-matrixtest <- matrix(0,nrow=length(varList),ncol=length(varList))
-colnames(matrixtest)<-names(varList)
-rownames(matrixtest)<-names(varList)
+finalMatrix <- matrix(0,nrow=length(varList),ncol=length(varList))
+#names(varList) might be able to be subsitute by myVars 
+colnames(finalMatrix)<-myVars
+rownames(finalMatrix)<-myVars
 
 #begin implementing calculation
 #TODO: find a way to find pairs of variables
 #3 cases
+#2 variables with empty matricies = 1 variable with a matricie
+#1 variable with empty 1 varibale with filled matrix = 1 variable 
+# 2 variables with filled matrix
 
-https://stackoverflow.com/questions/28056727/obtain-key-from-value-in-r
+
+#generation of matrixList
+#possible to fuse in previous for loop?
+matrixList<-list()
+for(i in 1:length(varList))
+{
+  matrixList[[i]]<- list(var=names(varList[i]), value=varList[[i]], varMatrix=matrix(0,nrow=length(varList),ncol=1))
+  rownames(matrixList[[i]]$varMatrix)<-myVars
+}
+
+#finding highest priority and amount
+varVal <- sapply(matrixList,"[[","value")
+maxPriority<-varVal[which.max(abs(varVal))]
+
+
+#ordering names in order
+nameStore<-myVars[order(varVal, decreasing=TRUE)]
+count<-0
+
+#find appropriate indexes to modify
+#TODO: maybe add a conditional statement to make sure numbers are even
+for(i in 1:sum(varVal==maxPriority))
+{
+  print(match(nameStore[i],myVars))
+}
+
+
+
